9f874fb8d03c462fc3ffad733f8b2e3e5b8bb11f
Allan Jiang <origami789@gmail.com>
2020-04-07 16:49:06 -0700

    handle tabs

--- js/dev.js
+++ js/dev.js
@@ -2,9 +2,19 @@ if (location.hostname === 'localhost') {
   document.write('<script src="http://localhost:35729/livereload.js?snipver=1"></script>');
 
   var cleanup = function() {
+    // handle brave
     $('script[data-dapp-detection=""]').remove()
+
+    // handle livereload
     $('script[src*="livereload.js"]').remove()
+
+    // handle top nav
     $('nav.is-fixed').removeClass('is-fixed')
+
+    // handle tabs
+    $('a[data-toggle="tab"]').removeClass('active').removeAttr('aria-expanded')
+    $('div.tab-pane.fade').removeClass('active show').removeAttr('aria-expanded')
+    $('div.tab-pane.fade').first().addClass('active show')
   }
 
   var edit = () => {
