7460e6a068132da152da096cfde62d4bafae2099
Allan Jiang <origami789@gmail.com>
2020-07-03 17:30:50 -0700

    all works

--- brochure2/script.js
+++ brochure2/script.js
@@ -7,13 +7,40 @@ $(function() {
     $('.active').removeClass('active')
     $card.addClass('active')
   })
-  window.fold = function(i) {
+  var fold = function(i) {
     $('.active').removeClass('active')
     $('.card-'+i).addClass('active')
     $('.card-'+i).removeClass('is-flipped')
   }
-  window.unfold = function(i) {
+  var unfold = function(i) {
     $('.active').removeClass('active')
-    $('.card-'+i).addClass('is-flipped', 'active')
+    $('.card-'+i).addClass('is-flipped')
+    $('.card-'+i).addClass('active')
+  }
+
+  var viewCnt = 0
+  var animMillis = 1000
+  window.fold = fold
+  window.unfold = unfold
+  window.nextView = function() {
+    viewCnt += 1
+    switch (viewCnt % 4) {
+      case 0:
+        fold(2)
+        setTimeout(() => fold(1), animMillis/3)
+        break;
+      case 1:
+        unfold(1)
+        break;
+      case 2:
+        unfold(3)
+        unfold(4)
+        break;
+      case 3:
+        fold(3)
+        fold(4)
+        setTimeout(() => unfold(2), animMillis)
+        break;
+    }
   }
 })
