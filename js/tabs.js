bb55f88ce4d84d7793869944854b1bc53cac1b26
Allan Jiang <origami789@gmail.com>
2020-09-17 12:32:27 -0700

    fix about page

--- js/tabs.js
+++ js/tabs.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
+  $('<style>'+
+    'a.active[data-toggle="tab"] {'+
+    '  background-color: #f0f8ff;'+
+    '}'+
+    'a[data-toggle="tab"] {'+
+    '  width: 100%;'+
+    '}'+
+    'a[data-toggle="tab"] li {'+
+    '  text-align: left;'+
+    '}'+
+    'ul.typography li {'+
+    '  margin: 10px 0px;'+
+    '}'+
+    'a.obvious {'+
+    '  color: #007bff;'+
+    '}'+
+    '</style>').appendTo('head')
   if (location.hash !== '') {
-    $('a[href="' + location.hash + '"]').tab('show');
+    $('a[data-toggle="tab"]').removeClass('active');
+    $('a[data-toggle="tab"][href="' + location.hash + '"]').tab('show').addClass('active');
   }
 
+  /*if(!location.hash) {
+    location.hash = $('a[data-toggle="tab"]').attr('href').substr(1)
+  }*/
   return $('a[data-toggle="tab"]').on('click', function(e) {
-    return location.hash = $(e.target.parentElement).attr('href').substr(1);
+    location.hash = $(e.target).closest('a').attr('href').substr(1);
   });
 });
